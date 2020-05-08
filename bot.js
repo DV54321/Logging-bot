8d00dd2a49e7617d104c57a9913bbf57981e56de
dvsazei <danielviola4457@gmail.com>
2020-05-08 13:26:13 -0400

    Updated with usernames included

--- bot.js
+++ bot.js
@@ -4,8 +4,8 @@ bot.on('ready', () => {
   console.log('Logged in and starting to log messages.')
 })
 bot.on('messageCreate', (message) => {
-  console.log(`Author: ${message.author}`)
-  console.log(`Channel: ${message.channel.id}`)
+  console.log(`Author: ${message.author} || ${message.author.username}`)
+  console.log(`Channel: ${message.channel.id} || ${message.channel.name}`)
   console.log(`Message: ${message.content}`)
 })
 bot.connect()
